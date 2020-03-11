d774d62bf01c0169c1dd9386c6571cc57bcf1c01
NataliaColome <nmcolome@gmail.com>
2020-03-11 03:09:57 -0600

    Add js to add sticky class for both navbars

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
-console.log(navbar)
+let navbars = document.getElementsByClassName("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+let sticky0 = navbars[0].offsetTop;
+let sticky1 = navbars[1].offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
+  let sticky = sticky0 > sticky1 ? sticky0 : sticky1
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbars[0].classList.add("sticky")
+    navbars[1].classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    navbars[0].classList.remove("sticky");
+    navbars[1].classList.remove("sticky");
   }
 }
